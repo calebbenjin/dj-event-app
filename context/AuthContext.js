10772479e87ab47a6373a49f3db50f927a7486e2
calebbenjin <calebben94@gmail.com>
2021-04-23 17:01:58 +0100

    Project-Setup

---
+++ context/AuthContext.js
@@ -0,0 +1,77 @@
+import { createContext, useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { NEXT_URL } from '@/lib/urls'
+
+const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+  const [error, setError] = useState(null)
+
+  const router = useRouter()
+
+  // useEffect(() => checkUserLoggedIn(), [])
+
+  // Register user
+  // const register = async (user) => {
+    
+  // }
+
+  // Login user
+  const login = async ({ email: identifier, password }) => {
+    const res = await fetch(`${NEXT_URL}/api/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        identifier,
+        password,
+      }),
+    })
+
+    const data = await res.json()
+
+    console.log(data)
+
+    if (res.ok) {
+      setUser(data.user)
+      // router.push('/account/dashboard')
+    } else {
+      setError(data.message)
+      setError(null)
+    }
+  }
+
+  // Logout user
+  // const logout = async () => {
+  //   const res = await fetch(`${NEXT_URL}/api/logout`, {
+  //     method: 'POST',
+  //   })
+
+  //   if (res.ok) {
+  //     setUser(null)
+  //     router.push('/')
+  //   }
+  // }
+
+  // Check if user is logged in
+  // const checkUserLoggedIn = async (user) => {
+  //   const res = await fetch(`${NEXT_URL}/api/user`)
+  //   const data = await res.json()
+
+  //   if (res.ok) {
+  //     setUser(data.user)
+  //   } else {
+  //     setUser(null)
+  //   }
+  // }
+
+  return (
+    <AuthContext.Provider value={{ user, error, login }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContext
